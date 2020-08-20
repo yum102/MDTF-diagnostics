9a342b9dfc28bdfaebe0ae97e52ab65f233bbd8e
Thomas Jackson <tom.jackson314@gmail.com>
2020-08-20 17:34:52 -0400

    Refactor old tests/ code
    NOAA-GFDL/MDTF-diagnostics#43
    
    Done to move shared_setup into framework/tests. The checksum code
    involved in this commit is currently unused and needs to be updated.

--- tests/make_file_checksums.py
+++ tests/make_file_checksums.py
@@ -4,30 +4,7 @@ import sys
 import argparse
 import textwrap
 import json
-import shared_test_utils as shared
-
-def checksum_in_subtree_1(rootdir, reference_subdirs, exclude_exts=[]):
-    # descend into subdirectories, then flatten file hierarchy
-    checksum_dict = {}
-    for d1 in reference_subdirs:
-        p1 = os.path.join(rootdir, d1)
-        assert os.path.isdir(p1)   
-        checksum_dict[d1] = shared.checksum_files_in_subtree(p1, exclude_exts)
-    return checksum_dict
-
-def make_output_data_dict(rootdir, case_list, exclude_exts=[]):
-    # same except we descend 2 levels deep and then flatten
-    checksum_dict = {}
-    for c in case_list:
-        d1 = c['dir']
-        p1 = os.path.join(rootdir, d1)
-        assert os.path.isdir(p1)   
-        checksum_dict[d1] = {}
-        for d2 in c['pod_list']:
-            p2 = os.path.join(rootdir, d1, d2)
-            assert os.path.isdir(p2) 
-            checksum_dict[d1][d2] = shared.checksum_files_in_subtree(p2, exclude_exts)
-    return checksum_dict
+from . import checksum_shared as shared
 
 
 if __name__ == '__main__':
@@ -51,19 +28,19 @@ if __name__ == '__main__':
     case_list = shared.get_test_data_configuration()
 
     print('Hashing input observational data')
-    checksum_dict = checksum_in_subtree_1(obs_path, case_list['pods'])
+    checksum_dict = shared.checksum_in_subtree_1(obs_path, case_list['pods'])
     with open(os.path.join(md5_path, 'checksum_obs_data.json'), 'w') as file_obj:
         file_obj.write(textwrap.dedent(header))
         json.dump(checksum_dict, file_obj)
 
     print('Hashing input model data')
-    checksum_dict = checksum_in_subtree_1(model_path, case_list['models'])
+    checksum_dict = shared.checksum_in_subtree_1(model_path, case_list['models'])
     with open(os.path.join(md5_path, 'checksum_model_data.json'), 'w') as file_obj:
         file_obj.write(textwrap.dedent(header))
         json.dump(checksum_dict, file_obj)
 
     print('Hashing output data')
-    checksum_dict = make_output_data_dict(out_path, case_list['case_list'],
+    checksum_dict = shared.make_output_data_dict(out_path, case_list['case_list'],
         ['.tar','.tar_old','.log','.json'])
     with open(os.path.join(md5_path, 'checksum_output.json'), 'w') as file_obj:
         file_obj.write(textwrap.dedent(header))
